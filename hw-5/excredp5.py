bc54c0c92cc20b92c92f974ba3f8643b1199078b
Ramaseshan Parthasarathy <tilluboy@gmail.com>
2017-11-29 12:58:35 -0500

    added extra credit problem

--- hw-5/excredp5.py
+++ hw-5/excredp5.py
@@ -1,68 +1,3 @@
-# Homework 5
-Ramaseshan Parthasarathy, Ashwin Sethi
-
-## Problem 1
-![](img/p1_horner.gif "horner")
-
-## Problem 2
-
-### Part A
-![](img/p2a.gif "mono") 
-
-### Part B
-![](img/p2b1.gif "lag")  
-![](img/p2b2.gif "lag2")  
-![](img/p2b3.gif "lag3")
-
-### Part C
-![](img/p2c.gif "newt")
-
-## Problem 3
-
-### Part A
-![](img/p3a1.gif "monomial")  
-![](img/p3a2.gif "monomial2")
-
-### Part B
-![](img/p3b1.gif "lagrange1")  
-![](img/p3b2.gif "lagrange2")  
-![](img/p3b3.gif "lagrange3")  
-![](img/p3b4.gif "lagrange4")
-
-### Part C
-
-#### Triangular Matrix
-![](img/p3ca1.gif "newton-one-one")  
-![](img/p3ca2.gif "newton-one-two")  
-![](img/p3ca3.gif "newton-one-three")  
-![](img/p3ca4.gif "newton-one-four")  
-![](img/p3ca5.gif "newton-one-five")
-
-#### Incremental Interpolation
-![](img/p3cb1.gif "newton-two-one")  
-![](img/p3cb2.gif "newton-two-two")  
-![](img/p3cb3.gif "newton-two-three")  
-![](img/p3cb4.gif "newton-two-four")  
-![](img/p3cb5.gif "newton-two-five")
-
-#### Divided Differences
-![](img/p3cc1.gif "newton-three-one")  
-![](img/p3cc2.gif "newton-three-two")  
-![](img/p3cc3.gif "newton-three-three") 
-
-## Problem 4
-
-### Part A
-![](img/p4a1.gif "four-one")    
-![](img/p4a2.gif "four-two")  
-![](img/p4a3.gif "four-three")
-
-### Part B
-![](img/p4b.gif "lag-four")    
-
-## Problem 5 (Extra Credit)
-
-```python
 import numpy as np
 
 def add(arg1, arg2): #helper method to add lists
@@ -115,7 +50,3 @@ def main():
     print('Derivative f3 = ', d3)
     print('Derivative Value @ x = 3 = ', dv3)
 main()
-```
-The program uses a recurrence relation to evaluate a polynomial f(x) at x = x<sub>0</sub>. The results of evaluation at x = 3 are shown below:  \
-![](img/p5ec.png "extra") 
-
